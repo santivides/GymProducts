17b70e67327c4fe834dc34602020f5420fbc7737
santivides <santivides01@gmail.com>
2024-06-25 18:19:11 -0500

    inicilazado repo local

---
+++ src/main/java/com/mycompany/gymproducts/GymProducts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymproducts;
+
+/**
+ *
+ * @author Santiago Vides
+ */
+public class GymProducts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
